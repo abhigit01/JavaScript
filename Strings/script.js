df978cc461fd4f9e8c9f599099bc9d08a23f61fa
abhigit01 <abhisheknke717@gmail.com>
2024-04-02 03:51:41 +0530

    add points

--- Strings/script.js
+++ Strings/script.js
@@ -16,6 +16,136 @@
 
 
 // Template literals
-let i= 5;
-let str3 = `this is template literals ${i}`
-console.log(str3)
+
+
+
+// let i= 5;
+// let str3 = `this is template literals ${i}`
+// console.log(str3)
+
+
+
+// example of template literales
+
+// let obj = {
+//     item : "pen",
+//     price : 10,
+// }
+// console.log(`the item is ${obj.item} and the price is ${obj.price} rupees`)
+
+
+
+
+
+
+
+
+// Escape Character
+
+// let str4= "Kreativan \n Technology"
+
+
+
+
+
+
+
+
+
+// tab space
+
+// let str5= "Kreativan \t Technology"
+
+
+
+
+
+
+
+// strings methods in JS
+
+
+// let str6 = "Hello I'm Learning JavaScript";
+// let str7 = str6.toUpperCase();
+// console.log(str7)
+
+
+
+
+
+
+
+// str toLowerCase
+
+
+// let str8 =str6.toLowerCase();
+// console.log(str8)
+
+
+
+
+
+
+
+
+// trim method for removing white spaces 
+
+
+// let str9 = "   in this no spaces are present     ";
+// let str10 = str9.trim();
+// console.log(str10)
+
+
+
+
+
+
+
+
+// slice method
+
+
+// let str11 = "hello";
+// console.log(str11.slice(1,3));
+
+
+
+
+
+
+
+
+// concat method ,, means add two strings
+
+// let str12= "hello";
+// let str13= " sir";
+// console.log(str12.concat(str13))
+// console.log(str12+str13);
+
+
+
+
+
+
+
+// Replace method
+
+// let str14 = "hello"
+// console.log(`the original string is ${str14} after replacement it will become help`)
+// console.log(str14.replace("lo","p"));
+
+
+// let str15 = "hellololo"
+// console.log(str15.replaceAll("lo","p"));
+
+
+
+
+
+
+// char at method
+
+
+// let str16 = "ILOVEJS";
+// console.log(str16[0]);
+
