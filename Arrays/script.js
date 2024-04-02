8770d09eff3bdc2cf15985f1280d21972b739a09
abhigit01 <abhisheknke717@gmail.com>
2024-04-03 01:44:28 +0530

    array

---
+++ Arrays/script.js
@@ -0,0 +1,120 @@
+// let marks = [97,54,88,78,55,82];
+// console.log(marks);
+
+
+// console.log(marks[3]);
+
+
+
+// string is immutable:----------------------------------------
+// arrays can be mutable:--------------------------------------
+
+// loopiong over an Array:-------------------------------------
+// print all element of an array:------------------------------
+
+
+
+
+
+
+// let studnts_name = ["rohan","sohan","ram","ajay","rishav"];
+// for(let i =0;i<=4;i++){
+//     console.log(studnts_name[i]);
+// }
+
+
+
+
+
+
+
+// anoher method for print the array element:-------------------------  for of  -------------------------------
+
+
+
+// let studnts_number = [97, 88,77, 55,68];
+// for(let num of studnts_number){
+//     console.log(num);
+// }
+
+
+// averageof the number :-------------------------------------------------------------------------------
+
+
+
+// let studnts_number2 = [97, 88,77, 55,68];
+// sum = 0;
+// for(let num of studnts_number ){
+//     sum= sum + num;
+// }
+// console.log(sum)
+
+
+
+
+// methods in array :-------------------------------------------------------------------
+
+
+// 1.    push() :---------------------add items in array from last index--------------------------------
+
+// let num = [1,2,3,4,5,6,7,8,];
+// console.log(num);
+// num.push(9,10);
+// console.log(`after push the array is  ${num}`);
+
+
+
+
+// 2.   pop() :------------------- Delete value from last index------------------------------------
+
+// let num2 = [1,2,3,4,5,6,7,8,];
+// let deletedvalue = num2.pop();
+// console.log(deletedvalue);
+
+
+// 3. toString() :-----------------------------------------------------------------------------------
+
+// let num3 = [1,2,3,4,5,6,7,8,];
+// let num4 = num3.toString();
+// console.log(num4);
+
+
+// 4. concat() method used for join two or more arrays :-----------------------------------------------
+
+// let color1 = ["red", "blue","green","yellow"];
+// let color2 = ["black", "pink","white","brown"];
+// let color = color1.concat(color2);
+// console.log(color);
+
+
+
+// 5. unshift method :---------------- work like push method but it's add element at the starting index of array---------------
+
+
+// 6. shift method :---------------- -work like pop method but it's delete element at the starting index of array---------------
+
+
+// 7.  slice method :---------------Returns some part of the array
+
+
+
+
+// let num5 = [1,2,3,4,5,6,7,8,];
+// console.log(num5.slice(1 , 4));
+
+
+
+
+
+// 8. splice method :------------ change original array (add, remove, replace)------------
+
+// splice(startldx,delCount,newElement1...)
+
+
+let arr = [1,2,3,4,5,6,7,8];
+arr.splice(8, 0, 9);
+console.log(arr);
+
+
+
+
